b4195297d1420a14bd912f28c774e062669ff4ac
Milan Minsky <milan@alum.mit.edu>
2014-03-30 12:02:17 -0400

    new file

---
+++ src/main/java/com/beartronics/jschema/SimpleSensoriMotorSystem.java
@@ -0,0 +1,29 @@
+package com.beartronics.jschema;
+// Basde on code examples from The Nature of Code
+// <http://www.shiffman.net/teaching/nature>
+// Spring 2011
+// PBox2D example
+
+// Basic example of falling rectangles
+
+import java.util.*;
+import processing.core.*;
+
+
+public class SimpleSensoriMotorSystem extends BaseSensoriMotorSystem {
+
+    public SimpleSensoriMotorSystem(JSchema a, PGraphics retina) {
+	super(a, retina);
+    }
+
+    public void processActions(WorldState w) {
+    }
+
+    public void stepPhysicalWorld() {
+
+    }
+
+
+}
+
+
